a58d16c2afa1c3b43bbaec4a50ddb031197788d3
BrandonWong <brandonwong@MacBook-Pro-Apple-M2-Pro.local>
2024-03-16 20:45:05 +0800

    build: Add buildSrc module to manage versions of dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.brandonhc.clocksampleapp"
-    compileSdk = 34
+    compileSdk = Versions.SDK_COMPILE_VERSION
 
     defaultConfig {
         applicationId = "com.brandonhc.clocksampleapp"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = Versions.SDK_MIN_VERSION
+        targetSdk = Versions.SDK_TARGET_VERSION
         versionCode = 1
         versionName = "1.0"
 
@@ -36,12 +36,18 @@ android {
 }
 
 dependencies {
+    /** AndroidX **/
+    implementation("androidx.core:core-ktx:${Versions.ANDROIDX_CORE_VERSION}")
+    implementation("androidx.appcompat:appcompat:${Versions.ANDROIDX_APPCOMPAT_VERSION}")
+    implementation("androidx.constraintlayout:constraintlayout:${Versions.ANDROIDX_CONSTRAINT_LAYOUT_VERSION}")
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    /** Google **/
+    implementation("com.google.android.material:material:${Versions.GOOGLE_MATERIAL_VERSION}")
+
+    /** Unit Test **/
+    testImplementation("junit:junit:${Versions.TEST_JUNIT_VERSION}")
+
+    /** UI Test **/
+    androidTestImplementation("androidx.test.ext:junit:${Versions.UI_TEST_JUNIT_VERSION}")
+    androidTestImplementation("androidx.test.espresso:espresso-core:${Versions.UI_TEST_ESPRESSO_VERSION}")
 }
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,25 @@
+object Versions {
+    /** Gradle **/
+    const val ANDROID_APPLICATION_GRADLE_PLUGIN_VERSION = "8.2.0"
+    const val KOTLIN_GRADLE_PLUGIN_VERSION = "1.9.21"
+
+    /** AndroidX SDK **/
+    const val SDK_COMPILE_VERSION = 34
+    const val SDK_TARGET_VERSION = 33
+    const val SDK_MIN_VERSION = 24
+
+    /** AndroidX **/
+    const val ANDROIDX_CORE_VERSION = "1.12.0"
+    const val ANDROIDX_APPCOMPAT_VERSION = "1.6.1"
+    const val ANDROIDX_CONSTRAINT_LAYOUT_VERSION = "2.1.4"
+
+    /** Google **/
+    const val GOOGLE_MATERIAL_VERSION = "1.11.0"
+
+    /** Unit Test **/
+    const val TEST_JUNIT_VERSION = "4.13.2"
+
+    /** UI Test **/
+    const val UI_TEST_JUNIT_VERSION = "1.1.5"
+    const val UI_TEST_ESPRESSO_VERSION = "3.5.1"
+}
